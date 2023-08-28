29ef611d5bf35cd1ec6333920152120434be746d
SuzGupta <suzskyer@gmail.com>
2023-08-28 13:09:21 -0700

    Initial Commit

---
+++ HabitRabbit/HabitRabbitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitRabbitApp.swift
+//  HabitRabbit
+//
+//  Created by Susannah Skyer Gupta on 8/28/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitRabbitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
