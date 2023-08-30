44e498b7d7a94e3ccd1f1c35c192bc09c404f822
SuzGupta <suzskyer@gmail.com>
2023-08-30 16:04:40 -0700

    can now add and display items

--- HabitRabbit/ContentView.swift
+++ HabitRabbit/ContentView.swift
@@ -6,27 +6,44 @@
 //
 
 import SwiftUI
+import Foundation
 
 struct ContentView: View {
   @StateObject var habits = Habits()
   @State private var showingAddHabit = false
 
   var body: some View {
-    VStack {
-      Text("What habit would you like to track?")
-        .padding()
-      Button("Add a habit") {
-        showingAddHabit = true
+    NavigationView {
+      VStack {
+        Text("My Habits")
+        List {
+          ForEach(habits.items) { habit in
+            VStack(alignment: .leading) {
+              Text(habit.name)
+                .font(.headline)
+              Text(habit.description)
+                .font(.subheadline)
+              Text("\(habit.amount)")
+                .font(.caption)
+            }
+          }
+        }
+        Text("What habit would you like to track?")
+          .padding()
+        Button("Add a habit") {
+          showingAddHabit = true
+        }
       }
-    }
-    .sheet(isPresented: $showingAddHabit) {
-      AddHabitView(habits: habits)
+      .sheet(isPresented: $showingAddHabit) {
+        AddHabitView(habits: habits)
+      }
+      .navigationTitle("Habit Rabbit 🐰")
     }
   }
 }
 
-struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
+  struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+      ContentView()
+    }
   }
-}
