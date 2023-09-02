5aea3786b5c8cc170056a4f0ba057702f043d3da
SuzGupta <suzskyer@gmail.com>
2023-09-01 18:13:33 -0700

    partway to the detail view with the help of the robot. lots of room for improvement (layout ugly, stepper not changing data)

---
+++ HabitRabbit/HabitDetailView.swift
@@ -0,0 +1,45 @@
+//
+//  HabitDetailView.swift
+//  HabitRabbit
+//
+//  Created by Susannah Skyer Gupta on 9/1/23.
+//
+
+import SwiftUI
+
+struct HabitDetailView: View {
+  @Binding var habit: Habit
+
+  var gradient: LinearGradient {
+    LinearGradient(gradient: Gradient(colors: [.purple,.blue]), startPoint: .topLeading, endPoint: .bottomTrailing)
+  }
+    var body: some View {
+      ZStack {
+        RoundedRectangle(cornerRadius: 25)
+          .fill(gradient)
+          .padding()
+        VStack(spacing: 20) {
+          VStack {
+            Text("\(habit.name)")
+              .font(.headline)
+              .foregroundColor(.white)
+            Text("\(habit.description)")
+              .font(.subheadline)
+              .foregroundColor(.white)
+          }
+          Stepper("Count: \(habit.amount)", value: $habit.amount)
+            .foregroundColor(.white)
+        }
+        .padding()
+      }
+
+    }
+}
+
+struct HabitDetailView_Previews: PreviewProvider {
+  static var sampleHabit = Habit(name: "Sample Habit", description: "This is a sample habit for preview purposes.", amount: 5)
+
+    static var previews: some View {
+      HabitDetailView(habit: .constant(sampleHabit))
+    }
+}
