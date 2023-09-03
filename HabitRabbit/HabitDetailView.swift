56398548bfde00f9a69738dbe19c3e322bea85ca
SuzGupta <suzskyer@gmail.com>
2023-09-03 14:02:35 -0700

    less ghastly, which is different than good

--- HabitRabbit/HabitDetailView.swift
+++ HabitRabbit/HabitDetailView.swift
@@ -13,33 +13,31 @@ struct HabitDetailView: View {
   var gradient: LinearGradient {
     LinearGradient(gradient: Gradient(colors: [.purple,.blue]), startPoint: .topLeading, endPoint: .bottomTrailing)
   }
-    var body: some View {
-      ZStack {
-        RoundedRectangle(cornerRadius: 25)
-          .fill(gradient)
-          .padding()
-        VStack(spacing: 20) {
-          VStack {
-            Text("\(habit.name)")
-              .font(.headline)
-              .foregroundColor(.white)
-            Text("\(habit.description)")
-              .font(.subheadline)
-              .foregroundColor(.white)
-          }
-          Stepper("Count: \(habit.amount)", value: $habit.amount)
-            .foregroundColor(.white)
-        }
+  var body: some View {
+    ZStack {
+      RoundedRectangle(cornerRadius: 25)
+        .fill(gradient)
         .padding()
+      VStack(spacing: 20) {
+        Text("\(habit.name)")
+          .font(.headline)
+          .foregroundColor(.white)
+        Text("\(habit.description)")
+          .font(.subheadline)
+          .foregroundColor(.white)
+        Stepper("Count: \(habit.amount)", value: $habit.amount)
+          .foregroundColor(.white)
+        // why aren't the stepper controls white?
+          .padding(.horizontal, 50)
       }
-
     }
+  }
 }
 
 struct HabitDetailView_Previews: PreviewProvider {
   static var sampleHabit = Habit(name: "Sample Habit", description: "This is a sample habit for preview purposes.", amount: 5)
 
-    static var previews: some View {
-      HabitDetailView(habit: .constant(sampleHabit))
-    }
+  static var previews: some View {
+    HabitDetailView(habit: .constant(sampleHabit))
+  }
 }
